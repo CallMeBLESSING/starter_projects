7c2e204f5e7231049d3648a9429bb88bc9a0706e
BLESSING <asubiojoblessing@gmail.com>
2021-05-28 20:15:08 +0300

    landing page updates

---
+++ landing_page/app/js/app.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('#hamburger');
+const header = document.querySelector('.header');
+hamburger.addEventListener('click', openMenu = () => {
+ hamburger.classList.toggle('open');
+})
