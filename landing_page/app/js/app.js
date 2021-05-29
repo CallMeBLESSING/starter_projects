f1a29c0d6eedd85319aacc6627b9333f6e08003e
BLESSING <asubiojoblessing@gmail.com>
2021-05-29 14:56:02 +0300

    updates

--- landing_page/app/js/app.js
+++ landing_page/app/js/app.js
@@ -1,5 +1,17 @@
 const hamburger = document.querySelector('#hamburger');
 const header = document.querySelector('.header');
-hamburger.addEventListener('click', openMenu = () => {
- hamburger.classList.toggle('open');
-})
+const overLay = document.querySelector('.overlay_menu');
+
+header.addEventListener('click', openMenu);
+function openMenu() {
+ header.classList.toggle('open');
+ if (header.classList.contains('open')) {
+  overLay.classList.add('fade_in');
+  overLay.classList.remove('fade_out');
+ } else {
+  overLay.classList.remove('fade_in');
+  overLay.classList.add('fade_out');
+ }
+}
+//
+
