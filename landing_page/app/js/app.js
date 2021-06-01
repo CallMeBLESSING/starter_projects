a4cee0b3e2d37260ba66589165e42e8a5ca674a8
BLESSING <asubiojoblessing@gmail.com>
2021-06-01 13:21:12 +0300

    updates

--- landing_page/app/js/app.js
+++ landing_page/app/js/app.js
@@ -1,17 +1,24 @@
 const hamburger = document.querySelector('#hamburger');
 const header = document.querySelector('.header');
 const overLay = document.querySelector('.overlay_menu');
+const fadeElements = document.querySelectorAll('.fade');
+const body = document.querySelector('body');
 
 header.addEventListener('click', openMenu);
 function openMenu() {
  header.classList.toggle('open');
- if (header.classList.contains('open')) {
-  overLay.classList.add('fade_in');
-  overLay.classList.remove('fade_out');
+ if(header.classList.contains('open')) {
+  body.classList.add('noScroll');
+  fadeElements.forEach(function(e){
+   e.classList.remove('fade_out');
+   e.classList.add('fade_in');
+  })
  } else {
-  overLay.classList.remove('fade_in');
-  overLay.classList.add('fade_out');
+  body.classList.remove('noScroll')
+  fadeElements.forEach(function(e){
+   e.classList.add('fade_out');
+   e.classList.remove('fade_in');
+  })
  }
 }
 //
-
