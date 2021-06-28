fe36d3acd6049e0321061ea72daaa28cb7ae48ba
BLESSING <asubiojoblessing@gmail.com>
2021-06-28 11:56:34 +0300

    finall update to sunshine page

--- project_files/app/src/app.js
+++ project_files/app/src/app.js
@@ -1,5 +1,8 @@
-const mobile = document.querySelector(".nav__mobile");
+const mobile = document.querySelector(".hamburger");
+const menu = document.querySelector(".mobile__menu");
 mobile.addEventListener('click', open);
 function open() {
  mobile.classList.toggle("openMobile");
+ menu.classList.toggle("open");
+
 }
